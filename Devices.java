83ede1dc1f53ba27be6705b7007c054f23692e32
Althea Gonzales <altheagonzalesctr@gmail.com>
2024-09-26 23:25:15 +0800

    Create Devices.java

---
+++ Devices.java
@@ -0,0 +1,19 @@
+public class Laptop {
+    public void charge() {
+        System.out.println("Laptop is charging.");
+    }
+}
+
+
+public class Refrigerator {
+    public void startCooling() {
+        System.out.println("Refrigerator is cooling.");
+    }
+}
+
+
+public class SmartphoneCharger {
+    public void chargePhone() {
+        System.out.println("Smartphone is charging.");
+    }
+}
